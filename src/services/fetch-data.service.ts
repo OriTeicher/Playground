f7a1e9b2c62d0fd67b40e89c0649081847abf707
OriTeicher <oriteicherudemy@gmail.com>
2024-07-29 00:43:21 +0300

    fetch testing

---
+++ src/services/fetch-data.service.ts
@@ -0,0 +1,39 @@
+import axios from "axios"
+import logger from "./logger.service"
+const fs = require("fs")
+const path = require("path")
+
+const JSON_PLACEHOLDER_URL = "https://jsonplaceholder.typicode.com/posts"
+
+export async function fetchData(
+  baseUrl: string = JSON_PLACEHOLDER_URL
+): Promise<any> {
+  try {
+    const response = await axios.get(baseUrl)
+    return response.data
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+export async function saveFetchedData(
+  caseNum: number,
+  url: string = JSON_PLACEHOLDER_URL,
+  filePath: string = `data/case_${caseNum}-saved-data.txt`
+): Promise<any> {
+  try {
+    const dir = path.dirname(filePath)
+    if (!fs.existsSync(dir)) {
+      console.log(`Directory '${dir}' does not exist. Creating it...`)
+      fs.mkdirSync(dir, { recursive: true })
+      console.log(`Directory '${dir}' created.`)
+    }
+    const data = await fetchData(url)
+    fs.writeFileSync(filePath, JSON.stringify(data, null, 2))
+    logger.info(`Data: ${data} \n saved to ${filePath}`)
+    return data
+  } catch (err) {
+    console.error("Error saving data:", err.message)
+    throw new Error(err.message)
+  }
+}
---
+++ src/tests/fetch-data.test.ts
@@ -0,0 +1,61 @@
+import axios from "axios"
+import { fetchData, saveFetchedData } from "../services/fetch-data.service"
+import logger from "../services/logger.service"
+
+jest.mock("axios")
+const mockedAxios = axios as jest.Mocked<typeof axios>
+
+// CASES TO TEST
+const TEST_CASE_1 = "fetchData should return data from API"
+const TEST_CASE_2 = "fetchData should be an array of at least 3 objects"
+const TEST_CASE_3 = "fetchData should be an array of exactly 5 objects"
+
+test(TEST_CASE_1, async () => {
+  const mockData = { data: "test data" }
+  mockedAxios.get.mockResolvedValue(mockData)
+  const data = await saveFetchedData(1)
+  expect(data).toEqual("test data")
+  logger.info(`Fetched data: ${JSON.stringify(data)}`)
+})
+
+test(TEST_CASE_2, async () => {
+  const mockData = {
+    data: [
+      { id: 1, value: "item 1" },
+      { id: 2, value: "item 2" },
+      { id: 3, value: "item 3" },
+    ],
+  }
+  mockedAxios.get.mockResolvedValue(mockData)
+  const data = await saveFetchedData(2)
+  expect(Array.isArray(data)).toBe(true)
+  expect(data.length).toBeGreaterThanOrEqual(3)
+  data.forEach((item) => {
+    expect(typeof item).toBe("object")
+    expect(item).not.toBeNull()
+  })
+
+  logger.info(`Fetched data: ${JSON.stringify(data)}`)
+})
+
+test(TEST_CASE_3, async () => {
+  const mockData = {
+    data: [
+      { id: 1, value: "item 1" },
+      { id: 2, value: "item 2" },
+      { id: 3, value: "item 3" },
+      { id: 4, value: "item 4" },
+      { id: 5, value: "item 5" },
+    ],
+  }
+  mockedAxios.get.mockResolvedValue(mockData)
+  const data = await saveFetchedData(3)
+  expect(Array.isArray(data)).toBe(true)
+  expect(data.length).toEqual(mockData.data.length)
+  data.forEach((item, idx) => {
+    logger.info(`item#${idx}: ${item} \n`)
+    expect(typeof item).toBe("object")
+    expect(item).not.toBeNull()
+  })
+  logger.info(`Fetched data: ${JSON.stringify(data)}`)
+})
